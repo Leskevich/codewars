74961c75aa9a2527dd97179f2d76a40d15997cae
leskevich <leskevichtema@gmail.com>
2022-12-20 11:59:56 +0100

    sum-

--- sumToLern.js
+++ sumToLern.js
@@ -6,21 +6,21 @@
 // return sum
 // }
 // console.log(sumTo(1))
-function sumTo1(s){
-    if(s===1){
-        return 1
-    }else {
-        return s + sumTo1(s-1)
-    }
-}
-console.log(sumTo1(3))
-function fib(n) {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-alert(fib(3)); // 2
-alert(fib(7)); // 13
-alert(fib(77));
+// function sumTo1(s){
+//     if(s===1){
+//         return 1
+//     }else {
+//         return s + sumTo1(s-1)
+//     }
+// }
+// console.log(sumTo1(3))
+// function fib(n) {
+//     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// }
+//
+// alert(fib(3)); // 2
+// alert(fib(7)); // 13
+// alert(fib(77));
 // //Напишите функцию printList(list), которая выводит элементы списка по одному.
 // let list = {
 //     value: 1,
@@ -49,3 +49,13 @@ alert(fib(77));
 //         printList(list.next)
 //     }
 // }
+// function printReverseListReturn(list) {
+//
+//     if (list.next) {
+//         printReverseListReturn(list.next);
+//     }
+//
+//     alert(list.value);
+// }
+//
+// printReverseListReturn(list);
